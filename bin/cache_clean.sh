8d24fa52c2892cb8d95e46e1f3ead0ddb453e6a3
Jon Stroop <jpstroop@gmail.com>
2012-10-02 11:53:29 -0400

    added a simple log to hold tallies

--- bin/cache_clean.sh
+++ bin/cache_clean.sh
@@ -8,13 +8,13 @@
 #
 # @author <jstroop@princeton.edu>
 #
-
+LOG="/tmp/loris_test/cache_clean.log"
 # Check that the cache directory
 CACHE_DIR="/tmp/loris_test/cache"
 
 # is below a certain size (in KB)
-# MAX_SIZE=524288000 #500 gb in kb
-REDUCE_TO=3072 #3 mb in kb
+# REDUCE_TO=524288000 #500 gb in kb
+REDUCE_TO=1048576 #1 gb in kb
 
 # and when it is larger, start deleting files accessed more than a certain 
 # number of days ago until the cache is smaller than the configured size.
@@ -28,14 +28,21 @@ current_usage () {
 	echo $(du -sk $CACHE_DIR | cut -f 1)
 }
 
+delete_total=0
 max_age=60 # days
 usage=$(current_usage)
+start_size=$usage
+run=1
 while [ $usage -gt $REDUCE_TO ] && [ $max_age -ge -1 ]; do
+	run=0
 	echo $max_age
 	echo $usage
 
-	# files.
-	find $CACHE_DIR -type f -atime +$max_age -delete
+	# files. loop (instead of -delete) so that we can keep count
+	for f in $(find $CACHE_DIR -type f -atime +$max_age); do
+		rm $f
+		let delete_total+=1
+	done
 
 	# broken symlinks
 	find -L $CACHE_DIR -type l -delete
@@ -46,5 +53,11 @@ while [ $usage -gt $REDUCE_TO ] && [ $max_age -ge -1 ]; do
 	let max_age-=1
 	usage=$(current_usage)
 done
+
+if [ $run == 0 ]; then
+	echo -ne "$(date +[%c cache_clean.sh]) Deleted $delete_count files to "
+	echo "get cache from $start_size kb to $usage kb." >> $LOG
+fi
+
 
 
