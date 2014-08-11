50dbba05c7f6d5f36bf4c4d4b335942cd4579e96
Jon Stroop <jpstroop@gmail.com>
2014-08-11 15:29:28 -0400

    Use ConfigObj for config files; rename loris.conf to loris2.conf. Closes #63, closes #105

--- loris/constants.py
+++ loris/constants.py
@@ -5,6 +5,8 @@ COMPLIANCE = 'http://iiif.io/api/image/2/level2.json'
 PROTOCOL = 'http://iiif.io/api/image'
 CONTEXT = 'http://iiif.io/api/image/2/context.json'
 
+CONFIG_FILE_NAME = 'loris2.conf'
+
 OPTIONAL_FEATURES = [
   'canonical_link_header',
   'mirroring',
