f205dcf7ba82cc9eac19d0ba6dff5d6c413deff2
Eliot Jordan <eliotj@princeton.edu>
2015-07-01 17:46:28 -0400

    fix loris2 config issues

--- loris/webapp.py
+++ loris/webapp.py
@@ -612,7 +612,7 @@ if __name__ == '__main__':
     extra_files = []
 
     project_dp = path.dirname(path.dirname(path.realpath(__file__)))
-    conf_fp = path.join(project_dp, 'etc', 'loris.conf')
+    conf_fp = path.join(project_dp, 'etc', 'loris2.conf')
     extra_files.append(conf_fp)
 
     sys.path.append(path.join(project_dp)) # to find any local resolvers
