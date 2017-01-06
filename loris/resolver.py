e9a33fcdc5c06713a3c10ef205d31be6ae121381
Ben Cail <benjamin_cail@brown.edu>
2017-01-06 10:53:08 -0500

    remove ExtensionNormalizingResolver - not needed anymore since that is done automatically for all resolvers

--- loris/resolver.py
+++ loris/resolver.py
@@ -111,24 +111,6 @@ class SimpleFSResolver(_AbstractResolver):
         return (source_fp, format_)
 
 
-# To use this the resolver stanza of the config will have to have both the
-# src_img_root as required by the SimpleFSResolver and also an
-# [[extension_map]], which will be a hash mapping found extensions to the
-# extensions that loris wants to see, e.g.
-#
-# [resolver]
-# impl = 'loris.resolver.ExtensionNormalizingFSResolver'
-# src_img_root = '/cnfs-ro/iiif/production/medusa-root' # r--
-#   [[extension_map]]
-#   jpeg = 'jpg'
-#   tiff = 'tif'
-# Note that case normalization happens before looking up in the extension_map.
-class ExtensionNormalizingFSResolver(SimpleFSResolver):
-    def __init__(self, config):
-        super(ExtensionNormalizingFSResolver, self).__init__(config)
-        self.extension_map = self.config['extension_map']
-
-
 class SimpleHTTPResolver(_AbstractResolver):
     '''
     Example resolver that one might use if image files were coming from
