45b08457e8fc840b6971780bcb07355a18caf40d
Rob Sanderson <azaroth42@gmail.com>
2017-08-04 12:31:40 -0700

    tests...

--- tests/parameters_t.py
+++ tests/parameters_t.py
@@ -40,8 +40,7 @@ class _ParameterTest(loris_t.LorisTest):
 		fmt = self.test_jp2_color_fmt
 		ident = self.test_jp2_color_id
 		uri = self.test_jp2_color_uri
-		ii = img_info.ImageInfo(uri, fp, fmt)
-		ii.from_image_file()
+		ii = img_info.ImageInfo(self.app, uri, fp, fmt)
 		return ii
 
 	def _get_info_long_x(self):
@@ -50,8 +49,7 @@ class _ParameterTest(loris_t.LorisTest):
 		fmt = self.test_jpeg_fmt
 		ident = self.test_jpeg_id
 		uri = self.test_jpeg_uri
-		ii = img_info.ImageInfo(uri, fp, fmt)
-		ii.from_image_file()
+		ii = img_info.ImageInfo(self.app, uri, fp, fmt)
 		return ii
 
 class TestRegionParameter(_ParameterTest):
