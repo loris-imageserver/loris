9e4298f369c0dde832681a7df50576e11e47b181
Alex Chan <alex@alexwlchan.net>
2017-09-25 11:58:41 +0100

    Get to 100% coverage in utils.py

---
+++ tests/utils_t.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8
+
+import os
+
+import mock
+import pytest
+
+from loris import utils
+
+
+class TestMkdirP:
+
+    def test_creates_directory(self, tmpdir):
+        path = str(tmpdir.join('test_creates_directory'))
+        assert not os.path.exists(path)
+
+        # If we create the directory, it springs into being
+        utils.mkdir_p(path)
+        assert os.path.exists(path)
+
+        # If we try to create the directory a second time, we don't throw
+        # an exception just because it already exists.
+        utils.mkdir_p(path)
+
+    def test_if_error_is_unexpected_then_is_raised(self, tmpdir):
+        """
+        If the error from ``os.makedirs()`` isn't because the directory
+        already exists, we get an error.
+        """
+        path = str(tmpdir.join('test_if_error_is_unexpected_then_is_raised'))
+
+        message = "Exception thrown in utils_t.py for TestMkdirP"
+
+        m = mock.Mock(side_effect=RuntimeError(message))
+        with mock.patch('loris.utils.os.makedirs', m):
+            with pytest.raises(RuntimeError) as err:
+                utils.mkdir_p(path)
+            assert err.value.message == message
+
