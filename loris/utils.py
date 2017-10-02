cf17089e1dd9881e78c6b7b44b55128ed72694bf
Alex Chan <alex@alexwlchan.net>
2017-10-02 15:22:02 +0100

    Get to 100% line coverage of img.py (#363)
    * Cover some missing lines in img.py
    
    * Move symlinking into utils.py, and test it thoroughly
    
    * Get to 100% line coverage in img.py

--- loris/utils.py
+++ loris/utils.py
@@ -3,11 +3,15 @@
 from __future__ import absolute_import
 
 import errno
+import logging
 import os
 
 
+logger = logging.getLogger(__name__)
+
+
 def mkdir_p(path):
-    '''Create a directory if it doesn't already exist.'''
+    """Create a directory if it doesn't already exist."""
     try:
         os.makedirs(path)
     except OSError as err:
@@ -15,3 +19,25 @@ def mkdir_p(path):
             pass
         else:
             raise
+
+
+def symlink(src, dst, force=True):
+    """Create a symlink from ``src`` to ``dst``.
+
+    Creates any required intermediate directories, and overrides any existing
+    file at ``dst``.
+
+    """
+    if src == dst:
+        logger.warn(
+            'Circular symlink requested from %s to %s; not creating symlink',
+            src, dst)
+        return
+
+    mkdir_p(os.path.dirname(dst))
+
+    # Shouldn't be the case, but helps with debugging.
+    if os.path.lexists(dst):
+        os.unlink(dst)
+
+    os.symlink(src, dst)
