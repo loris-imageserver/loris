16b22f6be62ec3d84a47f4529fc9cce5f2f0d109
Alex Chan <a.chan@wellcome.ac.uk>
2017-11-20 16:52:33 +0000

    Tidy up loris_exception.py

--- loris/loris_exception.py
+++ loris/loris_exception.py
@@ -1,37 +1,56 @@
-from __future__ import absolute_import
-
+# -*- encoding: utf-8 -*-
 
 class LorisException(Exception):
-    """Base exception class for Loris.
+    """Base exception class for all errors raised by Loris."""
+    pass
 
-    See <http://www-sul.stanford.edu/iiif/image-api/#error>
 
-    Attributes:
-        http_status (int): the HTTP status the should be sent with the response.
-        supplied_value (str): the parameter that caused the problem.
-        msg (str): any additional info about what went wrong.
+class HTTPException(Exception):
+    """Base class for exceptions that are returned as an HTTP response
+    to the user.
+
+    See http://iiif.io/api/image/2.1/#error-conditions.
+
+    :param http_status: The HTTP status that should be sent with the
+        associated HTTP response.
+    :type http_status: int
+    :param message: Information about the error.
+    :type msg: str
+
     """
-    def __init__(self, http_status=400, message=''):
-        """
-        Kwargs:
-            http_status (int): the HTTP status the should be sent with the
-                response.
-            msg (str): any additional info about what went wrong.
-            supplied_value (str): the parameter that caused the problem.
-        """
-        # message = '%s: %s (%d)' % (self.__class__.__name__, message, http_status)
-        super(LorisException, self).__init__(message)
+    def __init__(self, http_status, message):
+        super(HTTPException, self).__init__(message)
         self.http_status = http_status
 
-class SyntaxException(LorisException): pass
-class RequestException(LorisException): pass
-class ImageException(LorisException): pass
-class ImageInfoException(LorisException): pass
-class ResolverException(LorisException): pass
-class TransformException(LorisException): pass
-class AuthorizerException(LorisException): pass
+
+class SyntaxException(LorisException, HTTPException):
+    pass
+
+
+class RequestException(LorisException, HTTPException):
+    pass
+
+
+class ImageException(LorisException, HTTPException):
+    pass
+
+
+class ImageInfoException(LorisException, HTTPException):
+    pass
+
+
+class ResolverException(LorisException, HTTPException):
+    pass
+
+
+class TransformException(LorisException, HTTPException):
+    pass
+
+
+class AuthorizerException(LorisException, HTTPException):
+    pass
+
 
 class ConfigError(LorisException):
     """Raised for errors in the user config."""
-    def __init__(self, message):
-        super(ConfigError, self).__init__(http_status=None, message=message)
+    pass
