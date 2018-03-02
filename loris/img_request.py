0e3de15c148bd9083bb1e9e95a29331ee6722a11
Alex Chan <alex@alexwlchan.net>
2018-03-02 19:27:22 +0000

    img_request.py: restore the 'request_resolution_too_large' method

--- loris/img_request.py
+++ loris/img_request.py
@@ -9,6 +9,8 @@ except ImportError:  # Python 2
 
 import attr
 
+from loris.parameters import RegionParameter, SizeParameter
+
 
 @attr.s(slots=True)
 class ImageRequest(object):
@@ -55,3 +57,21 @@ class ImageRequest(object):
             ))
             self._request_path = '%s.%s' % (p, self.fmt)
         return self._request_path
+
+    def request_resolution_too_large(self, max_size_above_full, img_info):
+        if max_size_above_full == 0:
+            return False
+
+        region_param = RegionParameter(
+            uri_value=self.region_value,
+            img_info=img_info
+        )
+        size_param = SizeParameter(
+            uri_value=self.size_value,
+            region_parameter=region_param
+        )
+
+        max_width = region_param.pixel_w * max_size_above_full / 100
+        max_height = region_param.pixel_h * max_size_above_full / 100
+
+        return (size_param.w > max_width) or (size_param.h > max_height)
