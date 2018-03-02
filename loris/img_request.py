899519b3977eb743f8579b995eeb638c6eb12031
Alex Chan <alex@alexwlchan.net>
2018-03-02 19:47:16 +0000

    img_request.py: make size_param() a method

--- loris/img_request.py
+++ loris/img_request.py
@@ -63,15 +63,18 @@ class ImageRequest(object):
             img_info=img_info
         )
 
+    def size_param(self, img_info):
+        return SizeParameter(
+            uri_value=self.size_value,
+            region_parameter=self.region_param(img_info)
+        )
+
     def request_resolution_too_large(self, max_size_above_full, img_info):
         if max_size_above_full == 0:
             return False
 
         region_param = self.region_param(img_info=img_info)
-        size_param = SizeParameter(
-            uri_value=self.size_value,
-            region_parameter=region_param
-        )
+        size_param = self.size_param(img_info=img_info)
 
         max_width = region_param.pixel_w * max_size_above_full / 100
         max_height = region_param.pixel_h * max_size_above_full / 100
