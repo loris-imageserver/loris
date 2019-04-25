7ff506780cc495d6e25f54268ed3e57a2741dfd1
Ben Cail <benjamin_cail@brown.edu>
2019-04-25 13:53:17 -0400

    fix test for python 2

--- tests/user_commands_t.py
+++ tests/user_commands_t.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 import os
+import shutil
 import tempfile
 import unittest
 from configobj import ConfigObj
@@ -10,18 +11,22 @@ from loris import user_commands
 
 class TestCreateFilesAndDirectories(unittest.TestCase):
 
+    def tearDown(self):
+        shutil.rmtree(self.working_dir)
+
     def test_1(self):
         config = ConfigObj(user_commands._config_file_path(), unrepr=True, interpolation=False)
-        with tempfile.TemporaryDirectory() as working_dir:
-            image_cache = os.path.join(working_dir, 'image_cache')
-            info_cache = os.path.join(working_dir, 'info_cache')
-            log_dir = os.path.join(working_dir, 'logs')
-            www_dir = os.path.join(working_dir, 'www')
-            config['img.ImageCache']['cache_dp'] = image_cache
-            config['img_info.InfoCache']['cache_dp'] = info_cache
-            config['logging']['log_dir'] = log_dir
-            config['loris.Loris']['www_dp'] = www_dir
-            user_commands.create_default_files_and_directories(config)
-            for d in [image_cache, info_cache, log_dir, www_dir]:
-                self.assertTrue(os.path.exists(d))
+        #replace with TemporaryDirectory() when we can drop python 2
+        self.working_dir = tempfile.mkdtemp()
+        image_cache = os.path.join(self.working_dir, 'image_cache')
+        info_cache = os.path.join(self.working_dir, 'info_cache')
+        log_dir = os.path.join(self.working_dir, 'logs')
+        www_dir = os.path.join(self.working_dir, 'www')
+        config['img.ImageCache']['cache_dp'] = image_cache
+        config['img_info.InfoCache']['cache_dp'] = info_cache
+        config['logging']['log_dir'] = log_dir
+        config['loris.Loris']['www_dp'] = www_dir
+        user_commands.create_default_files_and_directories(config)
+        for d in [image_cache, info_cache, log_dir, www_dir]:
+            self.assertTrue(os.path.exists(d))
 
